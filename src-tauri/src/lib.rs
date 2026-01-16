a83fb612da1c0b0d135236f4f252ce7ce3440b00
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2026-01-16 18:39:48 +0530

    Added TitleBar and Transparency

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,8 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+use tauri::Manager;
+use window_vibrancy::apply_mica;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -7,6 +11,16 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            #[cfg(target_os = "windows")]
+            {
+                //TODO: Add a fallback for unsupported platforms
+                let _ = apply_mica(&window, None).expect("Unsupported Platform");
+                //TODO :Add option for mica on unsupported platform
+            }
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use window_vibrancy::apply_mica;
+
 fn main() {
     protege_lib::run()
 }
